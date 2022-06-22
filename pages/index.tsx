61a8c4d6713ee6102cda6b7804674a253dac1662
susukida <susukida@loremipsum.co.jp>
2022-06-23 08:51:20 +0900

    Add gist script

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,14 @@ import type { NextPage } from 'next'
 import {MouseEvent, useState} from 'react'
 
 const Navbar: NextPage = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false) // <1>
   const toggleDropdown = (event: MouseEvent) => {
     event.preventDefault()
     setIsOpen(!isOpen)
+  }
+
+  const hideDropdown = () => {
+    setIsOpen(false)
   }
 
   return (
@@ -18,19 +22,23 @@ const Navbar: NextPage = () => {
              id="navbarDropdown"
              role="button"
              aria-expanded={isOpen}
-             onClick={toggleDropdown}>
+             onClick={toggleDropdown}> {/* <2> */}
             Dropdown
           </a>
           <ul className={`dropdown-menu${isOpen ? ' show' : ''}`}
               style={{right: 0}}
               aria-labelledby="navbarDropdown">
             <li>
-              <a href="#action" className="dropdown-item">
+              <a href="#action"
+                 className="dropdown-item"
+                 onClick={hideDropdown}> {/* <3> */}
                 Action
               </a>
             </li>
             <li>
-              <a href="#another-action" className="dropdown-item">
+              <a href="#another-action"
+                 className="dropdown-item"
+                 onClick={hideDropdown}>
                 Another action
               </a>
             </li>
@@ -38,15 +46,18 @@ const Navbar: NextPage = () => {
               <hr className="dropdown-divider" />
             </li>
             <li>
-              <a href="#something-else-here" className="dropdown-item">
+              <a href="#something-else-here"
+                 className="dropdown-item"
+                 onClick={hideDropdown}>
                 Something else here
               </a>
             </li>
           </ul>
           {isOpen && (
-            <button className="modal-backdrop opacity-0"
+            <button type="button" // <4>
+                    className="modal-backdrop opacity-0"
                     style={{zIndex: 999, cursor: 'auto'}}
-                    onClick={toggleDropdown}>
+                    onClick={hideDropdown}>
               Hide dropdown
             </button>
           )}
